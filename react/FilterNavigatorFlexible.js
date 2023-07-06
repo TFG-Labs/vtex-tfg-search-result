14d653ef2ec82798c2fbad08ca7aaf98261ce5f5
Zizipho Retyu <giretyu>
2023-07-07 01:39:42 +0200

    tests

--- react/FilterNavigatorFlexible.js
+++ react/FilterNavigatorFlexible.js
@@ -1,74 +1,40 @@
-import React, { useMemo } from 'react'
+import React from 'react'
 import { useSearchPage } from 'vtex.search-page-context/SearchPageContext'
 import { useDevice } from 'vtex.device-detector'
 
 import FilterNavigator from './FilterNavigator'
-import FilterNavigatorContext from './components/FilterNavigatorContext'
 import styles from './searchResult.css'
-import { sortFilterValues } from './utils/sortFilterValues'
 
 const withSearchPageContextProps =
-  Component =>
+  () =>
   // eslint-disable-next-line react/display-name
-  ({
-    layout,
-    initiallyCollapsed,
-    scrollToTop,
-    maxItemsDepartment,
-    maxItemsCategory,
-    categoryFiltersMode,
-    filtersTitleHtmlTag,
-    truncateFilters,
-    openFiltersMode,
-    closeOnOutsideClick,
-    appliedFiltersOverview,
-    totalProductsOnMobile,
-    fullWidthOnMobile,
-    navigationTypeOnMobile,
-    updateOnFilterSelectionOnMobile,
-    drawerDirectionMobile,
-    showClearByFilter,
-    showClearAllFiltersOnDesktop,
-    priceRangeLayout,
-    facetOrdering = [],
-    showQuantityBadgeOnMobile = false,
-  }) => {
-    const {
-      searchQuery,
-      map,
-      params,
-      priceRange,
-      hiddenFacets,
-      filters,
-      showFacets,
-      preventRouteChange,
-      facetsLoading,
-    } = useSearchPage()
+  ({ layout }) => {
+    const { map, showFacets } = useSearchPage()
 
     const { isMobile } = useDevice()
 
-    const filtersFetchMore =
-      searchQuery && searchQuery.facets && searchQuery.facets.facetsFetchMore
-        ? searchQuery.facets.facetsFetchMore
-        : undefined
+    // const filtersFetchMore =
+    //   searchQuery && searchQuery.facets && searchQuery.facets.facetsFetchMore
+    //     ? searchQuery.facets.facetsFetchMore
+    //     : undefined
 
-    const facets =
-      searchQuery && searchQuery.data && searchQuery.data.facets
-        ? searchQuery.data.facets
-        : {}
+    // const facets =
+    //   searchQuery && searchQuery.data && searchQuery.data.facets
+    //     ? searchQuery.data.facets
+    //     : {}
 
-    const {
-      brands,
-      priceRanges,
-      specificationFilters,
-      categoriesTrees,
-      queryArgs,
-    } = facets
+    // const {
+    //   brands,
+    //   priceRanges,
+    //   specificationFilters,
+    //   categoriesTrees,
+    //   queryArgs,
+    // } = facets
 
-    const sortedFilters = useMemo(
-      () => sortFilterValues(filters, facetOrdering),
-      [filters, facetOrdering]
-    )
+    // const sortedFilters = useMemo(
+    //   () => sortFilterValues(filters, facetOrdering),
+    //   [filters, facetOrdering]
+    // )
 
     if (showFacets === false || !map) {
       return null
@@ -80,7 +46,7 @@ const withSearchPageContextProps =
           layout === 'desktop' && isMobile ? 'w-100 mh5' : ''
         }`}
       >
-        <FilterNavigatorContext.Provider value={queryArgs}>
+        {/* <FilterNavigatorContext.Provider value={queryArgs}>
           <Component
             preventRouteChange={preventRouteChange}
             brands={brands}
@@ -114,7 +80,7 @@ const withSearchPageContextProps =
             drawerDirectionMobile={drawerDirectionMobile}
             showQuantityBadgeOnMobile={showQuantityBadgeOnMobile}
           />
-        </FilterNavigatorContext.Provider>
+        </FilterNavigatorContext.Provider> */}
       </div>
     )
   }
