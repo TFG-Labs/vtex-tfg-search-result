10be027e6f442058321a84eb433aec930951da70
John Simms <johnbsimms@gmail.com>
2023-11-17 09:38:51 +0200

    Set up useSortFacetOptions hook

---
+++ react/hooks/useSortFacetOptions.js
@@ -0,0 +1,72 @@
+const clothingSizes = [
+  'xxs',
+  '2xs',
+  'xs',
+  's',
+  'm',
+  'l',
+  'xl',
+  'xxl',
+  '2xl',
+  'xxxl',
+  '3xl',
+  'xxxxl',
+  '4xl',
+  'xxxxxl',
+  '5xl',
+]
+
+const bedSizes = [
+  'single',
+  'three-quarter',
+  'double',
+  'queen',
+  'king',
+  'super-king',
+]
+
+const useSortFacetOptions = (facet, options) => {
+  if (facet === 'Size') {
+    // clothing sizes from 2xs to 5xl
+
+    const clothingSizesOptions = options.filter(option =>
+      clothingSizes.includes(option.name.toLowerCase())
+    )
+
+    const bedSizeOptions = options.filter(option =>
+      bedSizes.includes(option.name.toLowerCase())
+    )
+
+    const otherSizeOptions = options.filter(
+      option =>
+        ![...clothingSizes, ...bedSizes].includes(option.name.toLowerCase())
+    )
+
+    otherSizeOptions.sort((a, b) => {
+      // remove non-digits from start of string (eg. ±40cm)
+      const aName = parseFloat(a.name.replace(/^\D+/g, ''))
+      const bName = parseFloat(b.name.replace(/^\D+/g, ''))
+
+      if (aName < bName) return -1
+      if (aName > bName) return 1
+
+      return 0
+    })
+
+    // sort clothing sizes by index in clothingSizes array
+    clothingSizesOptions.sort(
+      (a, b) => clothingSizes.indexOf(a.name) - clothingSizes.indexOf(b.name)
+    )
+
+    // sort bed sizes by index in bedSizes array
+    bedSizeOptions.sort(
+      (a, b) => bedSizes.indexOf(a.name) - bedSizes.indexOf(b.name)
+    )
+
+    return [...clothingSizesOptions, ...bedSizeOptions, ...otherSizeOptions]
+  }
+
+  return options
+}
+
+export default useSortFacetOptions
